bcce2ff09a1977700cf6cb496ab101841f6c6b75
Musho-hub <Rasmus.s.o.kristensen@hotmail.com>
2025-11-28 11:20:02 +0100

    chore(types): extend Note and Tag types to include tag relationships

--- notes_app_frontend/src/lib/types.ts
+++ notes_app_frontend/src/lib/types.ts
@@ -1,13 +1,19 @@
-export interface Note {    // Notes
+export interface Note {             // Notes
     id: number;
     title: string;
     content: string;
     created_at: string;
     owner: number;
     owner_username: string;
+    tags: number[];
 }
 
 export interface TokenResponse {    // Auth (JWT)
     access: string;
     refresh: string;
+}
+
+export interface Tag {              // Tags
+    id: number,
+    name: string,
 }
